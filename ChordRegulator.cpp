e74e8bf2b262b33f687167d2d6812667bf1c03ce
Leekai <leekai.dev@gmail.com>
2024-03-31 01:42:08 +0300

    start refactor for my C++ lib

---
+++ ChordRegulator.cpp
@@ -0,0 +1,193 @@
+#include <regulator/ChordRegulator.h>
+
+ChordRegulator::ChordRegulator()
+{
+}
+
+void ChordRegulator::InitNh() // rclcpp::Node::SharedPtr nh
+{
+    // node = nh;
+    // cb_grp_client_ = ChordRegulator::node->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+    // trajectory_viz_pub = node->create_publisher<visualization_msgs::msg::Marker>("chord_regulator/trajectory/viz", 10);
+    // leading_vector_viz_pub = node->create_publisher<visualization_msgs::msg::Marker>("chord_regulator/leading_vector/viz", 10);
+}
+
+// Функция группировки путевых точек в вектор хорд
+int ChordRegulator::WaypointVectorToChordVector(std::vector<Eigen::Vector3d> waypoint_vector)
+{
+    ChordRegulator::chord_list_id = 0;
+    ChordRegulator::up_index = 0;
+    ChordRegulator::chords_vector = std::vector<std::vector<Eigen::Vector3d>>();
+    // Собираем пары путевых точек в хорды
+    for (int i = 0; i < waypoint_vector.size() - 1; ++i)
+    {
+        std::vector<Eigen::Vector3d> chord;
+        chord.push_back(waypoint_vector[i]);
+        chord.push_back(waypoint_vector[i + 1]);
+        ChordRegulator::chords_vector.push_back(chord);
+    }
+    // Находим хорды с длиной равной приблизительно 0.0 и удаляем их из последовательности
+    // for(int i = 0; i < ChordRegulator::chords_vector.size(); ++i)
+    // {
+    //     if (LengthOfChord(ChordRegulator::chords_vector[i]) < accepteble_chord_length)
+    //     {
+    //         auto begin_index = ChordRegulator::chords_vector.cbegin();
+    //         ChordRegulator::chords_vector.erase(begin_index + i);
+    //     }
+    // }
+    // if (ChordRegulator::chords_vector.size() > 0)
+    //     return true;
+    // else
+    return ChordRegulator::chords_vector.size();
+}
+
+// Вычисляем длину хорды
+float ChordRegulator::LengthOfChord(std::vector<Eigen::Vector3d> chord)
+{
+    return (chord[1] - chord[0]).norm();
+}
+
+// Находим проекцию точки на отрезок
+Eigen::Vector3d ChordRegulator::PointProjCoordsOnSegment3D(Eigen::Vector3d point, Eigen::Vector3d segment_start, Eigen::Vector3d segment_end)
+{
+    Eigen::Vector3d segment_direction = segment_end - segment_start;
+    double segment_length = segment_direction.norm();
+    segment_direction.normalize();
+    Eigen::Vector3d point_direction = point - segment_start;
+    double point_distance = point_direction.dot(segment_direction);
+
+    if (point_distance < 0)
+    {
+        return segment_start;
+    }
+    else if (point_distance > segment_length)
+    {
+        return segment_end;
+    }
+    else
+    {
+        return segment_start + segment_direction * point_distance;
+    }
+}
+
+// // Получаем маркер рисующий вектор в виде стрелки в RVIZ2
+// visualization_msgs::msg::Marker ChordRegulator::GetVectorMarker(std::vector<Eigen::Vector3d> chord,
+//                                                                 int id,
+//                                                                 int color_code)
+// {
+//     visualization_msgs::msg::Marker marker;
+//     marker.header.frame_id = "world";
+//     marker.header.stamp = ChordRegulator::node->get_clock()->now();
+//     marker.ns = "chords";
+//     marker.id = id;
+//     marker.type = visualization_msgs::msg::Marker::ARROW;
+//     marker.action = visualization_msgs::msg::Marker::ADD;
+//     geometry_msgs::msg::Point start_arrow;
+//     start_arrow.x = chord[0][0];
+//     start_arrow.y = chord[0][1];
+//     start_arrow.z = chord[0][2];
+//     geometry_msgs::msg::Point end_arrow;
+//     end_arrow.x = chord[1][0];
+//     end_arrow.y = chord[1][1];
+//     end_arrow.z = chord[1][2];
+//     marker.points.push_back(start_arrow);
+//     marker.points.push_back(end_arrow);
+//     if (color_code == 0)
+//     {
+//         marker.color.a = 1.0;
+//         marker.color.r = 1.0;
+//         marker.color.g = 0.0;
+//         marker.color.b = 0.0;
+//     }
+//     else
+//     {
+//         marker.color.a = 1.0;
+//         marker.color.r = 0.0;
+//         marker.color.g = 1.0;
+//         marker.color.b = 0.0;
+//     }
+//     marker.scale.x = 0.1;
+//     marker.scale.y = 0.2;
+
+//     return marker;
+// }
+
+// Возвращаем ведущий вектор в локальной системе координат робота
+Eigen::Vector3d ChordRegulator::GetLeadingVector()
+{
+    // for (int i = 0; i < ChordRegulator::chords_vector.size(); ++i)
+    // {
+    //     ChordRegulator::trajectory_viz_pub->publish(ChordRegulator::GetVectorMarker(ChordRegulator::chords_vector[i], i, 0));
+    // }
+
+    Eigen::Vector3d leading_vector;
+    if (ChordRegulator::chords_vector.size() > 0)
+    {
+        std::vector<Eigen::Vector3d> chord = ChordRegulator::chords_vector[ChordRegulator::chord_list_id];
+        Eigen::Vector3d pose_projection_on_chord = ChordRegulator::PointProjCoordsOnSegment3D(ChordRegulator::robot_pose, chord[0], chord[1]);
+        // Находим локальные координаты хорды относительно робота
+        std::vector<Eigen::Vector3d> local_chord;
+        local_chord.push_back(chord[0] - ChordRegulator::robot_pose);
+        local_chord.push_back(chord[1] - ChordRegulator::robot_pose);
+        // Находим локальные координаты проекции позиции робота на отрезок относительно робота
+        ChordRegulator::dist_to_chord = (pose_projection_on_chord - ChordRegulator::robot_pose).norm();
+
+        // Вычисляем полную длину маршрута
+        ChordRegulator::full_length_path = 0.0;
+        ChordRegulator::full_length_path = ChordRegulator::dist_to_chord + (chord[1] - pose_projection_on_chord).norm();
+        for (int i = ChordRegulator::chord_list_id; i < ChordRegulator::chords_vector.size() - 1; ++i)
+        {
+            ChordRegulator::full_length_path += (ChordRegulator::chords_vector[i + 1][1] - ChordRegulator::chords_vector[i + 1][0]).norm();
+        }
+
+        Eigen::Vector3d local_pose_projection_on_chord = pose_projection_on_chord - ChordRegulator::robot_pose;
+        // Находим ведущий вектор. Этот вектор управляет движением робота - куда двигаться вдоль прямой
+        if (ChordRegulator::dist_to_chord < ChordRegulator::min_dist_to_chord) // Если робот расположен ближе чем минимальное растояние от робота к проекции позиции робота на хорду переходим от наводящего вектора на хорду к направляющему вектору на конец хорды
+        {
+            // double length = (local_chord[1] - local_pose_projection_on_chord).norm();
+            // if (length > 5)
+            // {
+            //     Eigen::Vector3d vector_on_end_line = (local_chord[1] - local_pose_projection_on_chord) * 5 / length;
+            //     vector_on_end_line = vector_on_end_line + local_pose_projection_on_chord;
+
+            //     float k = ChordRegulator::dist_to_chord / ChordRegulator::min_dist_to_chord;
+            //     leading_vector = vector_on_end_line - local_pose_projection_on_chord + k * (local_pose_projection_on_chord - (vector_on_end_line - local_pose_projection_on_chord)); //- (local_chord[1] - local_pose_projection_on_chord)
+            // }
+            // else
+            // {
+                float k = ChordRegulator::dist_to_chord / ChordRegulator::min_dist_to_chord;
+                leading_vector = local_chord[1] - local_pose_projection_on_chord + k * (local_pose_projection_on_chord - (local_chord[1] - local_pose_projection_on_chord)); //- (local_chord[1] - local_pose_projection_on_chord)
+            // }
+            // float k = ChordRegulator::dist_to_chord / ChordRegulator::min_dist_to_chord;
+            // leading_vector = local_chord[1] - local_pose_projection_on_chord + k * (local_pose_projection_on_chord - (local_chord[1] - local_pose_projection_on_chord)); //- (local_chord[1] - local_pose_projection_on_chord)
+        }
+        else // Если робот расположен дальше чем минимальное расстояние от робота к проекции позиции робота на хорду, то следуем по наводящему вектору к проекции позиции робота на хорду
+        {
+            leading_vector = local_pose_projection_on_chord;
+        }
+        // Проверяем условие переключения между отрезками
+        if ((pose_projection_on_chord - chord[1]).norm() < ChordRegulator::min_dist_to_chord && ChordRegulator::chord_list_id < ChordRegulator::chords_vector.size() - 1)
+        {
+            ChordRegulator::chord_list_id += 1;
+        }
+        else if (ChordRegulator::chord_list_id >= ChordRegulator::chords_vector.size() - 1 && local_chord[1].norm() < ChordRegulator::min_dist_to_chord)
+        {
+            ChordRegulator::up_index = 1;
+        }
+        // Отображаем направляющий вектор в RVIZ в глобальной системе координат
+        std::vector<Eigen::Vector3d> local_leading_vector;
+        local_leading_vector.push_back(ChordRegulator::robot_pose);
+        local_leading_vector.push_back(leading_vector + ChordRegulator::robot_pose);
+        // ChordRegulator::leading_vector_viz_pub->publish(ChordRegulator::GetVectorMarker(local_leading_vector, 500, 1));
+    }
+    else
+    {
+        std::cout << "No chords!" << std::endl;
+    }
+    leading_vector = leading_vector.normalized() * ChordRegulator::max_velocity;
+    return leading_vector;
+}
+
+float ChordRegulator::RadToDeg(float rad) { return rad * 180 / M_PI; }
+
+float ChordRegulator::DegToRad(float deg) { return deg * M_PI / 180; }
---
+++ ChordRegulator.h
@@ -0,0 +1,53 @@
+#ifndef CHORD_REGULATOR_H
+#define CHORD_REGULATOR_H
+
+#include <iostream>
+#include <vector>
+// #include "rclcpp/rclcpp.hpp"
+// #include "geometry_msgs/msg/point.hpp"
+#include "eigen3/Eigen/Dense"
+// #include "visualization_msgs/msg/marker.hpp"
+// #include "geometry_msgs/msg/point.hpp"
+#include "cmath"
+
+class ChordRegulator
+{
+    public:
+        // rclcpp::Node::SharedPtr node;
+        // rclcpp::CallbackGroup::SharedPtr cb_grp_client_;
+        // rclcpp::TimerBase::SharedPtr main_timer;
+        // rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr trajectory_viz_pub;           // Паблишер для отображения траектории в RVIZ2
+        // rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr leading_vector_viz_pub;       // Паблишер для отображения направляющего вектора в RVIZ2
+        // Параметры настроек
+        float accepteble_chord_length = 0.1;                        // Максимальный допуск пересечения точек для сортировки точек и группировки их в отрезки
+        float min_dist_to_chord = 4;                              // Расстояние срабатывания отклонения направляющего вектора при следовании к траектории
+        float max_velocity = 2.5;
+        // Глобальные переменные
+        float full_length_path = 0.0;
+        int up_index = 0;
+        Eigen::Vector3d robot_pose;                                 // Координаты робота выраженные в трехмерном векторе
+        int chord_list_id = 0;                                      // id текущей хорды на исполнении
+        float dist_to_chord = 0.0;                                  // Дистанция к хорде на исполнении
+        float dist_to_chord_end = 0.0;                              // Дистанция к точек переключения между хордами
+        std::vector<std::vector<Eigen::Vector3d>> chords_vector;    // Вектор состоящий из прямых имеющих начало и конец
+        // Конструктор
+        ChordRegulator();
+        void InitNh(); // rclcpp::Node::SharedPtr nh
+        // Функция группировки путевых точек в вектор хорд
+        int WaypointVectorToChordVector(std::vector<Eigen::Vector3d> waypoint_vector);
+        // Вычисляем длину хорды
+        float LengthOfChord(std::vector<Eigen::Vector3d> chord);
+        // Получаем проекцию точки на ОТРЕЗОК
+        Eigen::Vector3d PointProjCoordsOnSegment3D(Eigen::Vector3d point, Eigen::Vector3d segment_start, Eigen::Vector3d segment_end);
+        // Получаем маркер рисующий вектор в виде стрелки в RVIZ2
+        // visualization_msgs::msg::Marker GetVectorMarker(std::vector<Eigen::Vector3d> chord,  
+        //                                                 int id,
+        //                                                 int color_code);
+
+        Eigen::Vector3d GetLeadingVector();
+
+        float RadToDeg(float rad); 
+
+        float DegToRad(float deg);
+};
+#endif //CHORD_REGULATOR_H
